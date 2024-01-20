7273a5f86f1bc2a413c5fe7058b9e8d39ea89f8b
Ryan Gang <ryan-gg@outlook.com>
2024-01-20 20:07:27 +0000

    Implement basic login.

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -21,7 +21,8 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
     end
     follow_redirect!
     assert_template "users/show"
+    assert is_logged_in?
     assert_not flash.empty?
-    assert_equal flash[:success], 'Welcome to the Sample App!'
+    assert_equal flash[:success], "Welcome to the Sample App!"
   end
 end
